274ac7283000a4cf8e21d9cfb8e9f720f0b1fd9b
Brendan Eamer <brendan.eamer@gmail.com>
2020-03-15 13:24:40 -0700

    Updates version

--- lib/slack-ruby-bot-server/api/endpoints/movr_endpoint.rb
+++ lib/slack-ruby-bot-server/api/endpoints/movr_endpoint.rb
@@ -13,14 +13,12 @@ module SlackRubyBotServer
           requires :workout_name, type: String, desc: 'Workout Name'
         end
         get do
-          puts "I GOT HERE"
           team = Team.where(challenge_code: params[:challenge_code]).first
           token = team.activated_user_access_token
           token = team.token
-          puts "Token: #{token}"
           client = Slack::Web::Client.new(token: token)
           #result = client.users_conversations(user: "U0Q5725HC")
-          client.chat_postMessage(channel: "D0100MNEBEU", text: "#{params[:name]} just completed #{params[:workout_name]}!!")
+          client.chat_postMessage(channel: team.movr_channel, text: "#{params[:name]} just completed #{params[:workout_name]}!!")
           /
           list = client.conversations_list.inspect
           puts client.bots_info.inspect
